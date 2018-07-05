ba833b9c7be4445a05d73830830d64bf246466f9
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-07-05 11:14:32 +1000

    upgrade pdqtest

--- examples/run.pp
+++ examples/run.pp
@@ -1,12 +1,12 @@
 # @PDQTest
 include download_and_do
 download_and_do::run { "test.sh":
-  source       => "/cut/spec/fixtures/test.sh",
+  source       => "/testcase/spec/fixtures/test.sh",
   user         => "bob",
   download_dir => "/usr/download",
 }
 
 # second run to detect duplicate resources
 download_and_do::run { "test2.sh":
-  source => "/cut/spec/fixtures/test2.sh",
+  source => "/testcase/spec/fixtures/test2.sh",
 }
