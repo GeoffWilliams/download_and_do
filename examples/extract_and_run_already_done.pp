2d471bd303dda89b7392d3eea8291ff3cd8cb940
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-08-21 11:58:56 +1000

    move all mocks out of fixtures directory to prevent getting lost by .gitignore, fix rspec tests to take account of new requirements supporting windows

--- examples/extract_and_run_already_done.pp
+++ examples/extract_and_run_already_done.pp
@@ -1,14 +1,14 @@
 # @PDQTest
 include download_and_do
 download_and_do::extract_and_run { "test.tar.gz":
-  source       => "/testcase/spec/fixtures/test.tar.gz",
+  source       => "/testcase/spec/mock/test.tar.gz",
   run_relative => "cp test.txt /extract_and_run_test",
   creates      => "/alreadydone",
 }
 
 # second run to detect duplicate resources
 download_and_do::extract_and_run { "test2.tar.gz":
-  source       => "/testcase/spec/fixtures/test2.tar.gz",
+  source       => "/testcase/spec/mock/test2.tar.gz",
   run_relative => "ls",
   creates      => "/alreadydone",
 }
--- examples/run_already_done.pp
+++ examples/run_already_done.pp
@@ -1,14 +1,14 @@
 # @PDQTest
 include download_and_do
 download_and_do::run { "test.sh":
-  source  => "/testcase/spec/fixtures/test.sh",
+  source  => "/testcase/spec/mock/test.sh",
   creates => "/alreadydone",
 
 }
 
 # second run to detect duplicate resources
 download_and_do::run { "test2.sh":
-  source  => "/testcase/spec/fixtures/test2.sh",
+  source  => "/testcase/spec/mock/test2.sh",
   creates => "/alreadydone",
 
 }
